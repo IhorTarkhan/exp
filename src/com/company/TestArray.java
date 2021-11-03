10f4186aa8f3b87a96661886ec0a61571d4d9151
Ihor.Tarkhan <ihor.tarkhan@lafox.net>
2021-11-03 15:54:17 +0200

    aaa

--- src/com/company/TestArray.java
+++ src/com/company/TestArray.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class TestArray {
     public static void main(String[] args) {
-        System.out.println("test Arr");
+        System.out.println("test Array 2");
     }
 }
